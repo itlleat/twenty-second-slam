c0e4339ba45e097b1acdf1ede5757f06cd17ddde
scott schulman <scott.schulman84@gmail.com>
2025-11-24 18:40:30 -0800

    added music to gameplay

--- scenes/levels/office.gd
+++ scenes/levels/office.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 func _ready():
 	print("Office level loaded")
-	
 	# Start the game timer
 	GameManager.start_game()
+	var audio_manager = get_node("GlobalAudioManager")
+	audio_manager.play_game_music()
