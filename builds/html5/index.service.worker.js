dd8086cde4689997265f6100b9f679724c958456
scott schulman <scott.schulman84@gmail.com>
2025-11-30 17:01:02 -0800

    parallax bg

--- builds/html5/index.service.worker.js
+++ builds/html5/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763856025|2744898';
+const CACHE_VERSION = '1764449665|7230699493';
 /** @type {string} */
 const CACHE_PREFIX = 'Twenty Second Sl-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
